8be4a190b1b7a615c06d8549fcfc86a58fcb31ef
Dan Zaratsian <dtz001@gmail.com>
2016-03-27 22:43:04 -0400

    Add ESP Scripts

--- intel/gateway/start_alerts.sh
+++
@@ -1,9 +0,0 @@
-
-
-export DFESP_HOME="/home/dtz001/sas/SASEventStreamProcessingEngine/3.1.0"
-export LD_LIBRARY_PATH="/home/dtz001/sas/SASEventStreamProcessingEngine/3.1.0/lib"
-
-
-$DFESP_HOME/bin/dfesp_xml_server -model file:////home/dtz001/sas/zprojects/intel/gateway/model_alerts.xml -http-admin 61061 -http-pubsub 61062 -pubsub 61063 -messages $DFESP_HOME/etc/xml/messages -loglevel esp=trace &
-
-
